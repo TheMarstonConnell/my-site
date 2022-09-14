461031278b4fde8cf42b1df2450eb25e5e4b7564
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-09-14 16:35:47 -0400

    fixed build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'planetminecraft.com',
+        port: '',
+        pathname: '/files/resource_media/screenshot/1834/logo-1535240013.png',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -27,73 +27,73 @@ export default function Home() {
           </h2>
 
           <div className={`${styles.grid} grid mb-12`}>
-            <a class="card w-96 bg-base-100 shadow-xl" href='https://www.curseforge.com/minecraft/mc-mods/random-loot-mod'>
+            <a className="card w-96 bg-base-100 shadow-xl" href='https://www.curseforge.com/minecraft/mc-mods/random-loot-mod'>
               <figure><img src="https://static.planetminecraft.com/files/resource_media/screenshot/1834/logo-1535240013.png" alt="Random Loot Logo" /></figure>
-              <div class="card-body">
-                <h2 class="card-title">
+              <div className="card-body">
+                <h2 className="card-title">
                   Random Loot Mod
                 </h2>
                 <p>Random Loot is a modification for Minecraft built with the Forge Modding API. The mod features new weapons and tools designed to emulate RPG loot.</p>
-                <div class="card-actions justify-end">
-                  <div class="badge badge-outline">Minecraft</div>
-                  <div class="badge badge-outline">Java</div>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">Minecraft</div>
+                  <div className="badge badge-outline">Java</div>
                 </div>
               </div>
             </a>
 
-            <a class="card w-96 bg-base-100 shadow-xl" href='https://jackaldao.com/'>
+            <a className="card w-96 bg-base-100 shadow-xl" href='https://jackaldao.com/'>
               <figure><img src="https://raw.githubusercontent.com/JackalLabs/canine-chain/master/banner.png" alt="Jackal Logo" /></figure>
-              <div class="card-body">
-                <h2 class="card-title">
+              <div className="card-body">
+                <h2 className="card-title">
                   Jackal
                 </h2>
-                <p>Jackal is a suite of privacy and cyber security tools built with Web3 technology. Jackal's flagship product is Jackal Storage, a cloud storage platform where even the hosts are unable to see your data.</p>
-                <div class="card-actions justify-end">
-                  <div class="badge badge-outline">Blockchain</div>
-                  <div class="badge badge-outline">Storage</div>
-                  <div class="badge badge-outline">Golang</div>
+                <p>Jackal is a suite of privacy and cyber security tools built with Web3 technology. Jackal&apos;s flagship product is Jackal Storage, a cloud storage platform where even the hosts are unable to see your data.</p>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">Blockchain</div>
+                  <div className="badge badge-outline">Storage</div>
+                  <div className="badge badge-outline">Golang</div>
                 </div>
               </div>
             </a>
 
-            <a class="card w-96 bg-base-100 shadow-xl" href='https://marstonc.itch.io/pathway-protectors'>
+            <a className="card w-96 bg-base-100 shadow-xl" href='https://marstonc.itch.io/pathway-protectors'>
               <figure><img src="https://img.itch.zone/aW1hZ2UvNTg2MTIwLzMwOTA3MDEucG5n/347x500/LQog%2FM.png" alt="Pathway Protectors Gameplay" /></figure>
-              <div class="card-body">
-                <h2 class="card-title">
+              <div className="card-body">
+                <h2 className="card-title">
                   Pathway Protectors
                 </h2>
                 <p>Pathway protectors is a simple tower defense game written for a 2 week game design project.</p>
-                <div class="card-actions justify-end">
-                  <div class="badge badge-outline">Game Design</div>
-                  <div class="badge badge-outline">Python</div>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">Game Design</div>
+                  <div className="badge badge-outline">Python</div>
                 </div>
               </div>
             </a>
 
-            <a class="card w-96 bg-base-100 shadow-xl" href='https://github.com/TheMarstonConnell/cookies'>
+            <a className="card w-96 bg-base-100 shadow-xl" href='https://github.com/TheMarstonConnell/cookies'>
               <figure><img src="https://handletheheat.com/wp-content/uploads/2020/10/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9-637x637-1.jpg" alt="Cookies" /></figure>
-              <div class="card-body">
-                <h2 class="card-title">
-                  Satoshi's Bakery
+              <div className="card-body">
+                <h2 className="card-title">
+                  Satoshi&apos;s Bakery
                 </h2>
                 <p>An idle clicker game built on the CosmosSDK inspired by Cookie Clicker</p>
-                <div class="card-actions justify-end">
-                  <div class="badge badge-outline">Blockchain</div>
-                  <div class="badge badge-outline">Golang</div>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">Blockchain</div>
+                  <div className="badge badge-outline">Golang</div>
                 </div>
               </div>
             </a>
 
-            <a class="card w-96 bg-base-100 shadow-xl" href='https://github.com/TheMarstonConnell/Hailstorm'>
+            <a className="card w-96 bg-base-100 shadow-xl" href='https://github.com/TheMarstonConnell/Hailstorm'>
               <figure><img src="https://pbs.twimg.com/profile_images/1263469903950106626/zCK-Zj1Q_400x400.png" alt="Hailstorm Logo" /></figure>
-              <div class="card-body">
-                <h2 class="card-title">
+              <div className="card-body">
+                <h2 className="card-title">
                   Hailstorm
                 </h2>
                 <p>A third-party Deezer front-end to make browsing for music a little bit better.</p>
-                <div class="card-actions justify-end">
-                  <div class="badge badge-outline">UX</div>
-                  <div class="badge badge-outline">Javascript</div>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">UX</div>
+                  <div className="badge badge-outline">Javascript</div>
                 </div>
               </div>
             </a>
